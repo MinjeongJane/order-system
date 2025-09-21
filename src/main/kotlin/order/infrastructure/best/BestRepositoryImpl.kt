64bddf420a979e0fd1cb2d85b850ce0eeb2543e5
minjeong <danceonair0@gmail.com>
2025-09-21 18:16:32 +0900

    resilience4j로 Redis fallback 구현하기

--- src/main/kotlin/order/infrastructure/best/BestRepositoryImpl.kt
+++ src/main/kotlin/order/infrastructure/best/BestRepositoryImpl.kt
@@ -1,14 +1,16 @@
 package order.infrastructure.best
 
 import com.querydsl.jpa.impl.JPAQueryFactory
-import java.time.Duration
-import java.time.LocalDate
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import order.api.dto.OrderDetailsRequest
 import order.domain.best.BestMenu
 import order.domain.best.BestRepository
 import order.domain.best.MenuOrderStatics
 import org.redisson.api.RedissonClient
 import org.springframework.stereotype.Repository
+import java.time.Duration
+import java.time.LocalDate
 
 @Repository
 class BestRepositoryImpl(
@@ -16,6 +18,8 @@ class BestRepositoryImpl(
     private val bestJpaRepository: BestJpaRepository,
     private val queryFactory: JPAQueryFactory,
 ) : BestRepository {
+    private val logger = KotlinLogging.logger {}
+
     override fun findBestMenuInRedis(): List<BestMenu> {
         val map = redissonClient.getMap<String, String>(BEST_MENU_KEY)
 
@@ -26,6 +30,7 @@ class BestRepositoryImpl(
             .map { BestMenu(it.key.toInt(), it.value.toInt()) }
     }
 
+    @RateLimiter(name = "dbFallbackLimiter", fallbackMethod = "fallbackToEmptyBestMenu")
     override fun getOrCacheBestMenu(): List<BestMenu> {
         val cached = findBestMenuInRedis()
         if (cached.isNotEmpty()) return cached
@@ -35,6 +40,11 @@ class BestRepositoryImpl(
             cacheBestMenuToRedis(dbResult) // 집계 결과를 Redis에 저장
         }
         return dbResult
+    }
+
+    fun fallbackToEmptyBestMenu(e: Throwable): List<BestMenu> {
+        logger.error(e) { "Redis fallback : BestMenu 조회 실패" }
+        return findBestMenuInDB()
     }
 
     private fun cacheBestMenuToRedis(menuIdCounts: List<BestMenu>) {
@@ -83,6 +93,11 @@ class BestRepositoryImpl(
             }
             updatedCount
         }
+    }
+
+    fun fallbackToDB(orders: List<OrderDetailsRequest>, e: Throwable): List<Long> {
+        recordMenuStatisticsBatch(orders)
+        return orders.map { it.count.toLong() }
     }
 
     override fun findMenuByDate(today: LocalDate, menuIds: List<Int>): List<MenuOrderStatics> =
